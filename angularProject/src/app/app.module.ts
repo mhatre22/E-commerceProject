e32967892ebf88c07834fa996fb644ebcab0f316
mhatre22 <mhatresnehal75@gmail.com>
2024-11-05 02:57:53 +0530

    fifth commit

--- angularProject/src/app/app.module.ts
+++ angularProject/src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
