228554e4eb439f77f381deec182428e7cc962ae5
mhatre22 <mhatresnehal75@gmail.com>
2024-12-02 11:18:52 +0530

    tenth commit

--- angularProject/src/app/app-routing.module.ts
+++ angularProject/src/app/app-routing.module.ts
@@ -4,20 +4,17 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 
-import { GroceryComponent } from './grocery/grocery.component';
-import { VegetableComponent } from './vegetable/vegetable.component';
-import { StaplesComponent } from './staples/staples.component';
-import { DairyproductComponent } from './dairyproduct/dairyproduct.component';
 import { SellersignupComponent } from './sellersignup/sellersignup.component';
 import { SellerloginComponent } from './sellerlogin/sellerlogin.component';
 import { SellerhomeComponent } from './sellerhome/sellerhome.component';
-import{sellerauthGuard} from './sellerauth.guard'
+ import{sellerauthGuard} from './sellerauth.guard'
 import { UserhomeComponent } from './userhome/userhome.component';
 import { userauthGuard } from './userauth.guard';
 import { MobilesComponent } from './mobiles/mobiles.component';
 import { MobilehomeComponent } from './mobilehome/mobilehome.component';
 import { SellerproductlistComponent } from './sellerproductlist/sellerproductlist.component';
 import { SelleraddproductComponent } from './selleraddproduct/selleraddproduct.component';
+import { SellerupdateProductComponent } from './sellerupdate-product/sellerupdate-product.component';
 const routes: Routes = [
   {
     path: 'login',component:LoginComponent
@@ -26,21 +23,16 @@ const routes: Routes = [
 {
     path: 'register',component:RegisterComponent
   },
-  { path: 'grocery',component:GroceryComponent,
-    children:[
-      {path:'vegetables', component:VegetableComponent},
-      {path:'staples', component:StaplesComponent},
-      {path:'dairyproducts', component:DairyproductComponent},
-    ]
-  },
+
   {path:'sellersignup',component:SellersignupComponent},
   {path:'sellerlogin',component:SellerloginComponent},
   {path:'sellerhome',component:SellerhomeComponent,canActivate:[sellerauthGuard]},
-  {path: 'userhome',component:UserhomeComponent,canActivate:[userauthGuard]},
+  {path:'userhome',component:UserhomeComponent,canActivate:[userauthGuard]},
   {path:'mobiles',component:MobilesComponent},
   {path:'mobilehome',component:MobilehomeComponent},
-  {path:'sellerproductlist',component:SellerproductlistComponent},
-  {path:'selleraddproduct',component:SelleraddproductComponent}
+  {path:'sellerproductlist',component:SellerproductlistComponent,canActivate:[sellerauthGuard]},
+  {path:'selleraddproduct',component:SelleraddproductComponent,canActivate:[sellerauthGuard]},
+  {path:'sellerupdateproduct/:id',component:SellerupdateProductComponent,canActivate:[sellerauthGuard]}
 
 
 ];
